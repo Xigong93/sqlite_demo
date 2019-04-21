4321024c0c0e2c3a270340ba1fdc5ec2db9ef053
pokercc <pokercc@sina.com>
2019-04-21 18:28:11 +0800

    成功了

--- main.cpp
+++ main.cpp
@@ -15,11 +15,16 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
 
 int main() {
     cout << "Hello, World!" << endl;
-    sqlite3 *db = NULL;
-    char *errorMessage = 0;
+    sqlite3 *db = nullptr;
+    char *errorMessage = nullptr;
     sqlite3_open("user.db", &db);
-    sqlite3_exec(db, "CREATE TABLE IS NOT EXISTS user( id integer , name varchar, age integer )", callback, 0, &errorMessage);
-    sqlite3_exec(db, "INERT INTO user(id,name,age) VALUES (1,'pokercc',8),(2,'kelly',9)", callback, 0, &errorMessage);
+    sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS user( id integer , name varchar, age integer )", callback, nullptr,
+                 &errorMessage);
+    cout << errorMessage << endl;
+    sqlite3_exec(db, "INSERT INTO user(id,name,age) VALUES (1,'pokercc',8),(2,'kelly',9)", callback, nullptr,
+                 &errorMessage);
+    cout << errorMessage << endl;
+
     sqlite3_close(db);
-    return 0;
+    return EXIT_SUCCESS;
 }
